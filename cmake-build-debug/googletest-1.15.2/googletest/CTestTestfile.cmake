ee41bf987aefbed2d143bde84a195e5cce3edc4f
BulliedDog <albertorovai1@gmail.com>
2024-08-30 15:43:39 +0200

    commit

---
+++ cmake-build-debug/googletest-1.15.2/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Lenovo/CLionProjects/a_star_sfml/googletest-1.15.2/googletest
+# Build directory: C:/Users/Lenovo/CLionProjects/a_star_sfml/cmake-build-debug/googletest-1.15.2/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
